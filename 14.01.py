0f3ec1cb0edaaf21dcf1cc6060b2a101ded50c33
Rodion Balaniuc <70860169+Rxdxxn@users.noreply.github.com>
2021-01-14 12:13:06 +0300

    Add files via upload

---
+++ 14.01.py
@@ -0,0 +1,36 @@
+with open('input.txt','r') as f:
+    a=f.readline()
+x=0
+d=0
+b=0
+c=0    
+for lit in a:
+with open('litereA.txt','w') as f:
+for i in range (0, len(n)):
+    if 90 >= ord >= 65:
+
+    f.write(str(x))    
+for lit in a:
+    if lit.islower():
+        d+=1   
+with open('litereB.txt','w') as f:
+    f.write(str(d))           
+for cifre in a:
+    if ord(cifre) in range(48,58):
+        c+=1 
+with open('operatori.txt','w') as f:
+    f.write(str(c)) 
+for i in a:
+    if ((ord(i) in range (32,48)) or (ord(i) in range(58,65)) or (ord(i) in range (91,9)) or (ord(i) in range (123,127))):
+        b+=1         
+with open('cifre.txt','w') as f:
+    f.write(str(b))     
+
+
+
+
+
+ 
+
+
+
